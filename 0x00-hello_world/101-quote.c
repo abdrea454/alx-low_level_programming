df3eda42f6e342f82a0bbfd588e81f21cfc1f80b
abdrea454 <emu024yesuf@gmail.com>
2022-12-14 12:12:54 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- *  * main - Entry point
- *   * Return: Always 1 (Success)
- *    */
+* main - Entry point
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
