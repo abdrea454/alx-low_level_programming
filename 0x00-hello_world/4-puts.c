fc1f5219b0813b44f54c5e9e57a501acb5ccb5a6
abdrea454 <emu024yesuf@gmail.com>
2022-12-14 01:17:18 -0800

    assembly code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,11 @@
  * Return: 0
  */
 int main (void)
-
-     {
-      
-          puts ("\"Programming is like building a multilingual puzzle");
-                return (0);
-     }		
-
+	{
+ 		puts("\"Programming is like building a multilingual puzzle");
+                	return(0);
+				
 	
- 
-  
+	   } 	
+	
+
