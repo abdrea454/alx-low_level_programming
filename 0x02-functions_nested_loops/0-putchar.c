14a0494c87e2b6e8393f8a9402f59808f981eccf
abdrea454 <emu024yesuf@gmail.com>
2022-12-14 15:11:36 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * is written by bwave
+ * Return: 0
+ */
+int main(void)
+{
+
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchr('0');
+	return (0);
+}
