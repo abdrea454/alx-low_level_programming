3d72848516b5072348d6f41d3a1ebeb14aa71d81
abdrea454 <emu024yesuf@gmail.com>
2022-12-14 15:25:09 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,14 @@
  */
 int main(void)
 {
-
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchr('0');
 	return (0);
+
 }
