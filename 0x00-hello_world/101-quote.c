0df9d414af67cee0a322c57cf3eaabdb4ba92e3f
abdrea454 <emu024yesuf@gmail.com>
2022-12-14 13:03:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,11 @@
 /**
 * main - prints exactly "and that piece of art is useful"
 * - Dora Korpar, 2015-10-19",
-* followed by a new line, to the standard error.   
+* followed by a new line, to the standard error.
 * Return: always 0 (success)
 */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-	\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
