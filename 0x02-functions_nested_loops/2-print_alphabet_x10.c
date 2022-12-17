8871a4cb329d6d2821eb3960ee10f3e9a5b53ab7
abdrea454 <emu024yesuf@gmail.com>
2022-12-17 13:36:45 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
+	int count;
 	char letter;
 
-	while (count++ <= 9)
+	for (count = 0; count <= 9; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
