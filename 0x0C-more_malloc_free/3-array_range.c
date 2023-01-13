3be6bf3c467435f4686035098b9b46036d1650da
abdrea454 <emu024yesuf@gmail,com>
2023-01-12 16:11:27 -0800

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,38 +1,31 @@
+#include "holberton.h"
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * *array_range - creates an array of integers
- * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements
+ * array_range - Creates an array of integers ordered
+ *               from min to max, inclusive.
+ * @min: The first value of the array.
+ * @max: The last value of the array.
  *
- * Return: pointer to the new array
+ * Return: If min > max or the function fails - NULL.
+ *         Otherwise - a pointer to the newly created array.
  */
 int *array_range(int min, int max)
 {
-	int *ptr;
-	int i, size;	
+	int *array, index, size;
+
 	if (min > max)
-
-	return (NULL);
-		size = max - min + 1;
-
-		ptr = malloc(sizeof(int) * size);
-		
-
-		
-	if (ptr == NULL)
-		
 		return (NULL);
-		
 
-		
-	for (i = 0; min <= max; i++)
-		
-		ptr[i] = min++;
-		
+	size = max - min + 1;
 
-		
-	return (ptr);
-		
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
+#ifndef MAIN_h
 #define MAIN_H
-
 
 #include <stdio.h>
 #include <stdlib.h>
